a9d32489f3a125d617a1f315811f03e87a4a6b3b
MarinaDonde <madondee@hotmail.com>
2024-02-18 11:49:43 -0300

    first commit

---
+++ src/main/java/com/ohmydog/OhMyDogApplication.java
@@ -0,0 +1,13 @@
+package com.ohmydog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhMyDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhMyDogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohmydog/OhMyDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohmydog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhMyDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
